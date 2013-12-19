e060eba1e7b5a06532bb089282021658b381beda
JamesKid <406358964@qq.com>
2013-12-19 11:59:35 +0800

    add book learn_plan other

---
+++ other/linux/from_web.sh
@@ -0,0 +1,23 @@
+# here the note of linux from the world wide web
+
+	1.tree
+			# description
+				this command can show the trees of the directory in the linux, 
+				you need to use "yum install tree" to install this in centos.
+
+			# exp:
+				tree			# show tree directilly 
+				tree --help		# show tree's command help
+				tree -N			# show tree's allow chinese language
+				tree -L 1		# just show the first level directory
+				tree -L 2		# just show two level directory 
+
+				
+
+
+
+
+
+
+
+
