4a4ef6b6a809166d51fd7cf8b019b20251485829
JamesKid <406358964@qq.com>
2014-01-03 15:41:11 +0800

    add nginx command

---
+++ other/web_server/nginx/nginx_command.sh
@@ -0,0 +1,7 @@
+1.restart nginx
+		# exp:
+		ps -auxww |grep nginx        # get the nginx process 
+		kill <process id>            # kill the process id 
+		/usr/local/nginx/sbin/nginx  # restart the nginx
+		
+	
