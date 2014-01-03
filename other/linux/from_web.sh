2df5ceadc032081c27b22dfc4ecd869e54d8d39d
JamesKid <406358964@qq.com>
2014-01-03 13:46:25 +0800

    add yum command description

--- other/linux/from_web.sh
+++ other/linux/from_web.sh
@@ -11,6 +11,30 @@
 				tree -N			# show tree's allow chinese language
 				tree -L 1		# just show the first level directory
 				tree -L 2		# just show two level directory 
+	2.yum	
+			# des:
+				this command can manage the soft
+			# exp:
+				yum install XXX     # install package
+				yum search XX	    # search the package
+				yum list	  	    # list all the can install package
+				yum list updates    # list all the package that can update
+				yum list installed  # list all the installed package
+				yum list extras     # list all not in yum repository's package
+				yum remove XXX      # remove package
+				yum list XXX        # list xxx package
+				yum info		    # show the package's information
+				yum info updates    # show the the update information
+				yum info installed  # show installed information
+				yum provides        # list at the package include
+				yum clean packages  # clean the /var/cache/yum 's package
+				yum clean headers   # clean the /var/cache/yum 's headers
+				yum clean oldheaders # clean the /var/cache/yum 's old headers
+				yum update			# update the system
+				yum deplist XXX     # check the dependency 
+				yum -y install php  # install php with no asking
+
+
 
 				
 
