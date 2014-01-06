5e511e25c58e729d4108838fea8623530e1f290e
JamesKid <406358964@qq.com>
2014-01-06 12:47:06 +0800

    add open 80 port

--- other/web_server/nginx/nginx_command.sh
+++ other/web_server/nginx/nginx_command.sh
@@ -1,7 +1,16 @@
-1.restart nginx
+# 1.restart nginx
 		# exp:
 		ps -auxww |grep nginx        # get the nginx process 
 		kill <process id>            # kill the process id 
 		/usr/local/nginx/sbin/nginx  # restart the nginx
+
+# 2.open the 80 port
+		# exp:
+		/sbin/iptables -I INPUT -p tcp --dport 80 -j ACCEPT  # open 80 port
+		/etc/rc.d/init.d/iptables save   # save the config
+		/etc/rc.d/init.d/iptables restart   #  restart the iptables service
+		/etc/init.d/iptables status    # show if the 80 port is open 
+
+	
 		
 	
