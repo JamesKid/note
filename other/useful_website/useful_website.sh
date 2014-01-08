79dbefbba4ecc43912dc8e03f963dfd03677d511
JamesKid <406358964@qq.com>
2014-01-08 17:30:39 +0800

    add website catory

--- other/useful_website/useful_website.sh
+++ other/useful_website/useful_website.sh
@@ -1,9 +1,18 @@
 #Authoer       :JamesKid
 #
-# web info
-	www.293.net     # get the value of the website
-	uptime.netcraft.com/up/graph?site=www.baidu.com   # get the information of the www.baidu.com
 
-# web server
-	http://www.anilcetin.com/convert-apache-htaccess-to-nginx/    # converapache htaccess rewrite rules to 
-																  # nginx
+
+
+# computer
+	# web info
+		http://www.293.net     # get the value of the website
+		http://uptime.netcraft.com/up/graph?site=www.baidu.com   # get the information of the www.baidu.com
+
+	# web server
+		http://www.anilcetin.com/convert-apache-htaccess-to-nginx/    # converapache htaccess rewrite 
+																 	  # rules to  nginx
+	# vim 
+		http://vimcasts.org/      # here are some moveis  intruduce the vim plugin
+	
+	# linux
+
