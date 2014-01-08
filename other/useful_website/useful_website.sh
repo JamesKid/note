6f902a61b11a2668c1e3abdb3632eaef93251995
JamesKid <406358964@qq.com>
2014-01-08 16:12:15 +0800

    add apache htacess rule to nginx rule

---
+++ other/useful_website/useful_website.sh
@@ -0,0 +1,9 @@
+#Authoer       :JamesKid
+#
+# web info
+	www.293.net     # get the value of the website
+	uptime.netcraft.com/up/graph?site=www.baidu.com   # get the information of the www.baidu.com
+
+# web server
+	http://www.anilcetin.com/convert-apache-htaccess-to-nginx/    # converapache htaccess rewrite rules to 
+																  # nginx
