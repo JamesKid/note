9e48121c21e2390cf869b58cd09986ac7ba9872a
JamesKid <406358964@qq.com>
2014-01-23 14:42:26 +0800

    add php.ini note ,add session fail

---
+++ other/php/2_php_ini_config_note.sh
@@ -0,0 +1,12 @@
+#====================================================
+# Description:  this is note about php.ini
+# Editor:       zhangshijie 
+# Email:        406358964@qq.com 
+# Version:      1.0 
+# History:      2014-01-23 creat# Date:   　　
+#====================================================
+
+# Fail
+	#	Unknown: Failed to write session data (files). Please verify that the 
+	#   current setting of session.save_path is correct () 
+		将php.ini里的session.save_path前加上分号,给与适当的权限
