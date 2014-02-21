14f756b4f0a594a9bbd6d4e75f703c3f35725f80
JamesKid <406358964@qq.com>
2014-02-21 11:54:08 +0800

    add php note

---
+++ other/php/php_note.sh
@@ -0,0 +1,16 @@
+#======================================================
+#!/bin/bash 
+# Description:  this program is note about php
+# Editor:       zhangshijie 
+ # Email:        406358964@qq.com 
+ # Version:      1.0 
+#======================================================
+# 
+
+
+# problem
+	# 中文乱码
+		在php文件头加入以下一句
+		header("Content-Type:text/html;charset=utf8");
+
+# error
