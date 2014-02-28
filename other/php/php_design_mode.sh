f0781a9bfb8686fe42776bec9cc6a35b369f96d9
JamesKid <406358964@qq.com>
2014-02-28 14:45:51 +0800

    add php_design_mode

---
+++ other/php/php_design_mode.sh
@@ -0,0 +1,9 @@
+#====================================================
+#!/bin/bash 
+# Description:  this is the note about php design mode 
+# Editor:       zhangshijie 
+# Email:        406358964@qq.com 
+# Version:      1.0 
+#====================================================
+
+
