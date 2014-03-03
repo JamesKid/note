43a460a703029473fe9ef5c0b5c450c24029139c
JamesKid <406358964@qq.com>
2014-03-03 16:14:22 +0800

    add vmware_note.sh

---
+++ other/vmware/vmware_note.sh
@@ -0,0 +1,12 @@
+#=================================================
+#!/bin/bash 
+# Description:  this program note about the vmware
+# Editor:       zhangshijie 
+# Email:        406358964@qq.com 
+# Version:      1.0 
+#=================================================
+# use tips 
+
+	# short cut 
+		1. ctrl+ alt    :   go back to windows , then you can use alt+tab in windows
+		2. ctrl+G		:   a little like click mouse left button 
