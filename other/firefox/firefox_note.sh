ae0e37e33935617a99791725316bf34e70a2210e
JamesKid <406358964@qq.com>
2014-03-05 10:08:41 +0800

    add firefox note

---
+++ other/firefox/firefox_note.sh
@@ -0,0 +1,11 @@
+# ================================JamesKid============================== 
+# @author		: JamesKid  
+# @time			: 2014_3_4
+# @description  : this is note about firefox
+# @fix			: 
+# ======================================================================
+# use tips 
+	# full screen 
+		press F11
+		right click menubar -> hidden the tags
+
