03024ba5ebfdbbaeac2612491cdaf48933a75e9b
JamesKid <406358964@qq.com>
2014-03-06 16:28:04 +0800

    add css beautiful web

---
+++ other/css/css_beautiful_web.sh
@@ -0,0 +1,10 @@
+# ================================JamesKid============================== 
+# @author		: JamesKid  
+# @description  : here are some beautiful css web 
+# @change		:
+# ======================================================================
+
+# css zend garden 
+	http://www.csszendgarden.com
+# media query
+	http://mediaqueri.es/
