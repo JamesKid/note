28406dcb55ce1b963949001b09549ed1df493642
JamesKid <406358964@qq.com>
2014-03-26 17:42:17 +0800

    add vim install in freebsd

--- other/unix/freebsd/use_tip.sh
+++ other/unix/freebsd/use_tip.sh
@@ -41,3 +41,11 @@
 					alias vi vim
 					setenv EDITOR vim
 				reboot
+			# method3
+				ftp ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2	# logs as anonymous
+				tar xvfz vim-7.4.tar.bz2		# unzip the file
+				cd /vim74
+				./configure
+				make
+				makeinstall
+				
