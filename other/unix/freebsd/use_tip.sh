a4bfcb273392cccd0d5a30ad67fbc4c5cb2456bf
JamesKid <406358964@qq.com>
2014-03-26 14:53:16 +0800

    add vim install and network setting in freebsd

--- other/unix/freebsd/use_tip.sh
+++ other/unix/freebsd/use_tip.sh
@@ -4,10 +4,40 @@
 # Email:        406358964@qq.com 
 # Version:      1.0 
 # History:      2014_1_27_17_12 add install vim
+#               2014_1_27_17_12 add net setting
 #================================================================
 # install 
-
+	# get 
+		# get it from web 
+	# network setting
+		# path 
+			
+# net
+	# command 
+		netstat -r    # show the route table
+		sh /etc/rc	  # restart thte network
+		/etc/netstart # restart thte network
+	# /etc/rc.conf
+		# ifconfig_em0="DHCP"
+		# ifconfig_em0="inet 192.168.72.130 netmask 255.255.255.0"
+		# defaultroute="192.168.72.255"
+		# route add default 192.168.72.2
+# path 
+	/etc/rc.conf		# set ip and route and host name
+	/etc/resolve.conf	# dns 
 # soft
+	
 	# vim
 		# install
-			pkg_add -r vim
+			# method1
+				pkg_add -r vim
+			# method2
+				cd /usr/ports/editors/vim
+				make install clean
+				# wait it download
+				cp /usr/local/share/vim/vim73/vimrc_example.vim ~/.vimrc
+				# comment the last two line in .vimrc
+				vi .cshrc
+					alias vi vim
+					setenv EDITOR vim
+				reboot
