ab226ca264cb5194ac4251fb25eb63b180fcec74
JamesKid <406358964@qq.com>
2014-04-15 11:44:56 +0800

    linux 系统管理技术手册note

---
+++ book/1_computer/6_linux/2_linux_basic_use/3_Linux系统管理技术手册.sh
@@ -0,0 +1,9 @@
+# ================================JamesKid============================== 
+# @author		: JamesKid  
+# @description : this is note about linux系统管理技术手册
+# @change		:
+# ======================================================================
+# 第1章 从何处入手
+	# command
+		rpm -q python		# check the version of python
+
