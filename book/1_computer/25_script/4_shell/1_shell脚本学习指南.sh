54b4b61db72dd978b7857e0b186b41ca401f689f
JamesKid <406358964@qq.com>
2014-04-15 18:02:57 +0800

    add shell脚本学习指南booknote

---
+++ book/1_computer/25_script/4_shell/1_shell脚本学习指南.sh
@@ -0,0 +1,12 @@
+# ================================JamesKid============================== 
+# @author		: JamesKid  
+# @description : this is note about 'shell脚本学习指南'
+# @change		:
+# ======================================================================
+# 第1章 背景知识
+# 第2章 入门  
+	# command
+		who | wc -l		# count how many line
+		who | wc -w		# count how many word
+		who | wc -c		# count how many character
+
