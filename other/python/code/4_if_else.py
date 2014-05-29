d221700d6ab1904411761d363baeb8a92eea0936
JamesKid <406358964@qq.com>
2014-05-29 17:12:06 +0800

    add python code file

---
+++ other/python/code/4_if_else.py
@@ -0,0 +1,19 @@
+# Multi-way decision 
+x=int(raw_input("please enter an integer:"))
+if x<0:
+	x=0 
+	print "Negative changed to zero"
+
+elif x==0:
+	print "Zero"
+
+else:
+	print "More"
+
+
+
+# Loos List
+a = ['cat','windows','defenestrate']
+for x in a :
+	print x,len(x)
+
---
+++ other/python/code/6_range_function.py
@@ -0,0 +1,9 @@
+# The range(0 function
+a = range(5,10)
+print a 
+a = range(-2,-7)
+print a
+a = range(-7,-2)
+print a 
+a = range(-2,-11,-3) # The 3rd parameter stands for step 
+print a 
