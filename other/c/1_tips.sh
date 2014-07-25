08e3d411bebf614e968e72c7c5ae95fd5c385705
JamesKid <406358964@qq.com>
2014-07-25 10:25:31 +0800

    add gcc -o name.out source.c

---
+++ other/c/1_tips.sh
@@ -0,0 +1,15 @@
+# ================================JamesKid============================== 
+# @author		: JamesKid  
+# @description : this is the note about c language and gcc
+# @change		:
+# ======================================================================
+
+
+
+# c
+
+# gcc
+	# use
+		# compile to the filename I want (生成指定的文件名)
+			gcc -o i_want.out test.c
+			gcc -o i_want.exe test.c
