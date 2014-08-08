457cb943cf017f4c52401cb10822c698c72bd1b3
JamesKid <406358964@qq.com>
2014-08-08 14:49:35 +0800

    add htop use

--- other/linux/soft.sh
+++ other/linux/soft.sh
@@ -126,6 +126,18 @@
 			./configure
 			make
 			make install
+		# use 
+			Shortcut	FunctionKey	 Description		         中文说明
+			h, ?			F1		 Invoke htop Help			 查看htop使用说明
+			S		        F2		 Htop Setup Menu		     htop 设定
+			/				F3		 Search for a Process	     搜索进程
+			\				F4		 Incremental process filtering	增量进程过滤器
+			t				F5		 Tree View				     显示树形结构
+			<, >			F6		 Sort by a column	         选择排序方式
+			[				F7		 Nice - (change priority)	 可减少nice值，这样就可以提高对应进程的优先级
+			]				F8		 Nice + (change priority)	 可增加nice值，这样就可以降低对应进程的优先级
+			k				F9		 Kill a Process				 可对进程传递信号
+			q				F10		 Quit htop					 结束htop
 		
 	# zabbix 
 	# nagios 
