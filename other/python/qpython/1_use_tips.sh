63425b3d35b13e93dd53238e1e272af44d8bf654
JamesKid <406358964@qq.com>
2014-10-11 15:33:56 +0800

    add use tips in qpython

---
+++ other/python/qpython/1_use_tips.sh
@@ -0,0 +1,10 @@
+========================================================
+#  this is note about qpython in android 
+=======================================================
+# use 
+	# let the script run in the console 
+		test.py
+			print("test")
+			#qpy:console
+
+
