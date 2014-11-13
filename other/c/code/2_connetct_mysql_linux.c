389dc1f3e7eee8227edf54809b18b45621319889
JamesKid <406358964@qq.com>
2014-11-13 17:49:52 +0800

    add c language link mysql in linus

---
+++ other/c/code/2_connetct_mysql_linux.c
@@ -0,0 +1,31 @@
+#include <stdio.h>  
+#include <mysql/mysql.h>/*注意要包含这个头文件*/
+/* yum install mysql-devel # */
+/* rpm -ql muysql-devel   # find the path of msyql.h */
+/* http://blog.csdn.net/fykhlp/article/details/5950485 */
+/* c 语言里function 要写在前面 */
+int main(int argc,char *argv[])  
+{  
+	MYSQL conn;  
+	int res;     
+	mysql_init(&conn);//初始化 连接  
+	if(mysql_real_connect(&conn,"localhost","root","123456","test",0,NULL,CLIENT_FOUND_ROWS))  
+	{  
+		printf("数据库连接成功/n");  
+		res = mysql_query(&conn,"insert into stucent values('user','123456')");//插入语句，成功返回0，失败返回1  
+		if(res)  
+		{  
+			printf("语句执行失败/n");  
+			mysql_close(&conn);//记得关闭连接  
+		}  
+		else  
+		{  
+			printf("语句执行成功/n");  
+			mysql_close(&conn);  
+		}  
+	}         
+	return 0;  
+}  
+
+/* 编译要下成面的语句才能成功 */
+/* gcc -o test_mysql test_mysql.c `mysql_config --cflags --libs` */
