d0946b936131a260795ebc803285102ca7cfa06d
JamesKid <406358964@qq.com>
2015-01-14 15:27:23 +0800

    add php extention install note

--- other/php/php_note.sh
+++ other/php/php_note.sh
@@ -15,8 +15,25 @@
 			# 若要空格的话要先将空格转了
 			$result =  str_replace(" ","&nbsp",$string);
 			$result = nl2br($result);
-			echo $result;
-		
+			echo $result;		
+# extention  (扩展)
+	# 动态添加扩展
+		#下载下应版本的源码包如(php-5.3.3.tar.gz)
+			#进入源码包需要的扩展
+			cd php-5.3.3/ext/gettext
+			/usr/local/php/bin/phpize
+			./configure --with-php-config=/usr/local/php/bin/php-config
+			make && make install
+			#　修改php.ini文件加入扩展
+				vim /etc/php.ini
+					# 添加一行
+					extention = gettext.so
+					extention_dir = "安装后提示的扩展目录"
+			# 生效
+				#　重启apache 或php-fpm即可生效
+			# 注意
+				#　必须使用相同版本的源码包安扩展,以避免版本不同的错误
+
 
 # problem
 	# 中文乱码
