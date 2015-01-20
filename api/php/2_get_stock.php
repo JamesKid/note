90e8fd81e2ef7169ee7ca2c623b391333df7ac99
JamesKid <406358964@qq.com>
2015-01-20 14:58:23 +0800

    add stock api

---
+++ api/php/2_get_stock.php
@@ -0,0 +1,19 @@
+<?php
+	/* this is api about stock 
+	 * learn : http://www.360doc.com/content/11/0921/21/6877178_150213502.shtml 
+	 *
+	 * use : 查询单个 http://hq.sinajs.cn/list=sh601003
+	 *		 查询多个 http://hq.sinajs.cn/list=sh601003,sh601001 
+	 *
+	 *		 sz: 深证
+	 *		 sh: 上证
+	 *
+	 * tips: 上证 sh000001
+	 *		 深成 sz399001
+	 *		 sz000783 长江证券(深证)
+	 *		 sh600691 阳煤化工(上证)
+	 *
+	 * options: 
+	 *
+	 */
+?>
