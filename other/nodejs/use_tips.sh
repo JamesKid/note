3b4e15ff84eac4306abb212f3a5b9af602176237
JamesKid <406358964@qq.com>
2015-02-06 17:43:30 +0800

    add nodejs note

--- other/nodejs/use_tips.sh
+++ other/nodejs/use_tips.sh
@@ -18,39 +18,63 @@
 			root@xxx#   node example.js
 		    firefox http://192.168.85.xx:1337 
 # language (语法)
-		# 引用模块 require
-			var http = require('http');
-			# 常用模块
-				fs   : 操作文件及文件系统
-				http : http服务器及客户端口
-		# 创建及调用模块
-			# 方法一
-				vim file.js
-					var hello = require('hello.js');
-					hello.world();
-				vim hello.js
-					exports.world = function() {
-						console.log('hello world');
-					}
-				# exports 是公开的模块接口
-				# require 获取模块的接口
-			#　方法二
-				# hello.js 
-				function Hello() { 
-					varname; 
-					this.setName = function(thyName) { 
-						name = thyName; 
+		# 模块
+			# 引用模块 require
+				var http = require('http');
+				# 常用模块
+					fs   : 操作文件及文件系统
+					http : http服务器及客户端口
+			# 创建及调用模块
+				# 方法一
+					vim file.js
+						var hello = require('hello.js');
+						hello.world();
+					vim hello.js
+						exports.world = function() {
+							console.log('hello world');
+						}
+					# exports 是公开的模块接口
+					# require 获取模块的接口
+				#　方法二
+					# hello.js 
+					function Hello() { 
+						varname; 
+						this.setName = function(thyName) { 
+							name = thyName; 
+						}; 
+						this.sayHello = function() { 
+							console.log('Hello ' + name); 
+						}; 
 					}; 
-					this.sayHello = function() { 
-						console.log('Hello ' + name); 
-					}; 
-				}; 
-				module.exports = Hello;
-				# main.js 
-				var Hello = require('./hello'); 
-				hello = new Hello(); 
-				hello.setName('BYVoid'); 
-				hello.sayHello(); 
+					module.exports = Hello;
+					# main.js 
+					var Hello = require('./hello'); 
+					hello = new Hello(); 
+					hello.setName('BYVoid'); 
+					hello.sayHello(); 
+		# 事件 (event)
+			
+		# 函数
+			function say(word) {
+				console.log(word);
+			}
+			function execute(someFunction, value) {
+				someFunction(value);
+			}
+			execute(say, "Hello");
+		# 路由
+		# 全局对象(global)
+			注意： 永远使用var 定义变量以避免引入全局变量，因为全局变量会污染 命名空间，提高代码的耦合风险。
+		# 文件系统
+			varfs = require('fs'); 
+			fs.readFile('content.txt', function(err, data) { 
+				if(err) { 
+					console.error(err); 
+				} else{ 
+					console.log(data); 
+				} 
+			}); 
+		# GET/POST请求
 
 
 # use 
