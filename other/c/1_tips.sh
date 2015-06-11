f048ac1d9acd9e6e8ec44d7e2034dded64cf7d12
JamesKid <406358964@qq.com>
2015-06-11 11:53:14 +0800

    add gcc use tips

--- other/c/1_tips.sh
+++ other/c/1_tips.sh
@@ -18,6 +18,29 @@
 			# compile to the filename I want (生成指定的文件名)
 			gcc -o i_want.out test.c
 			gcc -o i_want.exe test.c
+		# process 
+			# 1 预处理
+				gcc -E test.c -o test.i  或 gcc -E test.c
+			# 2 编译为汇编代码(Compilation)
+				gcc -S test.i -o test.s
+			# 3 汇编(Assembly)
+				gcc -c test.s -o test.o   # 些时可以直接 ./test.o 执行
+			# 4 链接 (Linking )
+				gcc test.o -o test 
+		# multy file compile
+			gcc test1.c test2.c -o test
+			# 以上命令等同以下命令
+				gcc -c test1.c -o test1.o
+				gcc -c test2.c -o test2.o
+.				gcc test1.o test2.o -o test
+		# 检错 (让程序与ANSI/ISO c标准兼容)
+			gcc -pedantic illcode.c -o illcode
+			gcc -Wall illcode.c -o illcode # 使GCC产生尽可能多的警告信息
+			gcc -Werror test.c -o test # GCC会在所有产生警告的地方停止编译，迫使程序员对自己的代码进行修改
+
+			
+
+
 
 # path 
 	# path of .h
