e58cc1a0073e1b0af9e994e5fd253bb7fc0ac148
JamesKid <406358964@qq.com>
2015-06-16 00:19:58 +0800

    add find latest Failed password

--- other/script/linux_server_script.sh
+++ other/script/linux_server_script.sh
@@ -5,3 +5,7 @@
 # ======================================================================
 	# find ip who use ssh password fail and times 
 		cat /var/log/secure | awk '/Failed/{print $(NF-3)}' | sort | uniq -c | awk '{print $2" = "$1;}'  #001 find paddword fail in ssh  from /secure
+
+	# find latest Failed password limit 100 line
+		cat /var/log/secure | grep 'Failed password' | tail -n 100  #002
+
