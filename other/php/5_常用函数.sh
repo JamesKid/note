2af221cbd5e8d08b4c0de6b0f2bf670f47e0cd34
JamesKid <406358964@qq.com>
2015-09-16 11:19:18 +0800

    add php常用函数

---
+++ other/php/5_常用函数.sh
@@ -0,0 +1,12 @@
+# 数组类
+		array_values  # 重新排列键值 array_values($array)	
+# 魔术方法
+		 __FUNCION__   # 获得方法名
+		 __CLASS__   # 获得类名
+		 __clone     # 复制实便
+		 __construct # 构造函数,实例创建时执行
+		 __destruct  # 析构函数,实例销毁时执行
+
+# 格式处理类
+		number_format   # 取小数点后几位 
+		round           # 四舍五入
