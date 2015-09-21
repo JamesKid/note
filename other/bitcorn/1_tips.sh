3f836511bf6632cac01813a2ae58bfec4a7e55df
JamesKid <406358964@qq.com>
2015-09-21 11:50:04 +0800

    add bitcoin note and code and website

---
+++ other/bitcorn/1_tips.sh
@@ -0,0 +1,9 @@
+# web
+		# bitcoin pholosophy(比特币运作原理)
+				http://blog.jobbole.com/53433
+# github code
+		# bitcoin code(比特币源代码) 
+				https://github.com/bitcoin/bitcoin
+		# bitcoin print code(比特币打印)
+				https://github.com/aristus/bitcoin-printer
+		
