7cc92d05decaedbe30cbf44e3b23fe515afaa6ef
JamesKid <406358964@qq.com>
2015-09-25 17:49:13 +0800

    add _SERVEr

--- other/php/5_常用函数.sh
+++ other/php/5_常用函数.sh
@@ -10,5 +10,28 @@
 # 格式处理类
 		number_format   # 取小数点后几位 
 		round           # 四舍五入
+		http_build_query # 数组转成url参数形式 key=value&key2=value3&key3=value3
 # 获取类
 		rand     # 获得随机数　 rand(10,100) 获得10到100的随机数
+
+# 服务器类
+		#　$_SERVER
+				$_SERVER[’PHP_SELF’]
+					http://www.yoursite.com/example/ — – — /example/index.php
+					http://www.yoursite.com/example/index.php — – — /example/index.php
+					http://www.yoursite.com/example/index.php?a=test — – — /example/index.php
+					http://www.yoursite.com/example/index.php/dir/test — – — /dir/test
+
+				$_SERVER['REQUEST_URI']
+					http://www.yoursite.com/example/ — – — /
+					http://www.yoursite.com/example/index.php — – — /example/index.php
+					http://www.yoursite.com/example/index.php?a=test — – — /example/index.php?a=test
+					http://www.yoursite.com/example/index.php/dir/test — – — /example/index.php/dir/test
+
+				$_SERVER['SCRIPT_NAME']
+					http://www.yoursite.com/example/ — – — /example/index.php
+					http://www.yoursite.com/example/index.php — – — /example/index.php
+					http://www.yoursite.com/example/index.php — – — /example/index.php
+					http://www.yoursite.com/example/index.php/dir/test — – — /example/index.php
+
+
