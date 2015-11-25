6890df3b19e624130855a15c460835557b8c21cf
JamesKid <406358964@qq.com>
2015-11-25 17:57:33 +0800

    add 禁止查看页面源泉代码

--- other/html/1_html.sh
+++ other/html/1_html.sh
@@ -7,3 +7,19 @@
 			http://alteredqualia.com
 # 1.页面html乱码处理方法    在head加下如下这句
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+# 禁止查看源代码
+	<head>
+	<script>
+	function stop(){
+		return false;
+	}
+	document.oncontextmenu=stop;
+	</script>
+	</head>
+	<body oncontextmenu=self.event.returnValue=false>
+	<script   language=javascript>
+	window.onselectstart=function()   {return   false;}       //禁用选择
+	window.oncopy=function()   {return   false;}       //禁止复制
+	</script> 
+	a;lkj;lkej
+	</body>
