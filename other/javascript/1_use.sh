bad367bbe0a4fe158cc3eb2d6296173ec302ea22
JamesKid <406358964@qq.com>
2015-12-03 15:03:01 +0800

    add javascript单例模式

--- other/javascript/1_use.sh
+++ other/javascript/1_use.sh
@@ -9,7 +9,8 @@
 		string2 = "lkjlkjlk";
 		string3 = string1+string2;
 	# function
-		
+# debug
+		cosole.log('test'); # 在firefox的控制台，全部下面查看
 # jquery
 	# 输出jquery版本
 		alert(jQuery.fn.jquery);      
@@ -18,3 +19,45 @@
 			$(this).next();
 		# 选择往下所有匹配的第一个 (选择第一个匹配class = conteent的)
 			$(this).nextAll(".content:eq(0)");
+
+# 模式
+		# 单例模式
+				<!doctype html>  
+				<html>  
+					<head lang="zh-CN">  
+					</head>
+					<body>
+						<script>
+						console.log('test5');
+						var SingletonTester = (function () { 
+								//参数：传递给单例的一个参数集合 
+								function Singleton(args) { 
+								console.log('mainfunc');
+										this.name = 'SingletonTester'; 
+										this.pointX = 6; 
+										this.pointY = 7; 
+								} 
+								//实例容器 
+								var instance; 
+								var _static = { 
+										name: 'SingletonTester', 
+										//返回Singleton的实例 
+										getInstance: function (args) { 
+												if (instance === undefined) { 
+														instance = new Singleton(args); 
+												} 
+												return instance; 
+										} ,
+										setName: function (args) { 
+									this.name = args;
+										} ,
+								}; 
+								return _static; 
+						})(); 
+						//var singletonTest = SingletonTester.getInstance({ pointX: 5 }); 
+						var test = SingletonTester.setName('abc'); 
+						var singletonTest2 = SingletonTester.name; 
+						console.log(singletonTest2); // 输出 5  
+						</script>
+					</body>  
+				</html>
