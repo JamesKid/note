27422bee53b65c2ebf8661e7ae328733da6973a4
JamesKid <406358964@qq.com>
2016-03-07 18:01:40 +0800

    add vbscript kill

--- other/secure/1_病毒查杀脚本/1_html_vbscript_kill/search.sh
+++ other/secure/1_病毒查杀脚本/1_html_vbscript_kill/search.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-grep -H -R -i 'vbs' * | grep 'html' > /tmp/result.txt  # 过滤含有vbs 的html
+grep -l -R -i 'keyword' .>/tmp/result.txt  # 列出相应文件路径
+#grep -H -R -i 'keyword' .>/tmp/result.txt  # 列出详情和路径
