fa8fa81c73e4b4406cc5715d7356b5d8d8b66bf0
JamesKid <406358964@qq.com>
2016-03-10 14:06:33 +0800

    add cloud.sagemath.com  数学云编辑软件

--- math/sagemath/1_use.sh
+++ math/sagemath/1_use.sh
@@ -3,4 +3,6 @@
 # @description :
 # @change				:
 # ======================================================================
-
+# web
+		# sagemath 云　(在线数学编辑,可用github登录) *******
+				https://cloud.sagemath.com   
