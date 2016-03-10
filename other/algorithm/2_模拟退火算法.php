1e7292dab3b66dfc0ce5af1a8eda14033dfc1b1f
JamesKid <406358964@qq.com>
2016-03-10 17:31:11 +0800

    add 算法

---
+++ other/algorithm/2_模拟退火算法.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+ * title:		 模拟退火算法
+ * description:  模拟退火算法
+ * web:          http://blog.csdn.net/acdreamers/article/details/10019849
+ * slove:       1. 费马点问题
+ *				2. 函数最值问题求解
+ *				3. tsp问题求解
+ *				4. 最小包含球问题
+ *
+ */
+?>
