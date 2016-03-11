74734ed4695b21dbc0367f490b3449b308ac41c7
JamesKid <406358964@qq.com>
2016-03-11 17:20:31 +0800

    add sage 使用教程

--- math/sagemath/1_use.sh
+++ math/sagemath/1_use.sh
@@ -4,5 +4,7 @@
 # @change				:
 # ======================================================================
 # web
+		# learn 
+				https://www.lainme.com/doku.php/topic/sage/start  # sage教程(good)
 		# sagemath 云　(在线数学编辑,可用github登录) *******
 				https://cloud.sagemath.com   
