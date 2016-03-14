4241761afa67b72622cfa81a7baa585d7e47bae3
JamesKid <406358964@qq.com>
2016-03-14 09:28:36 +0800

    add learn web

--- math/sagemath/1_use.sh
+++ math/sagemath/1_use.sh
@@ -6,5 +6,9 @@
 # web
 		# learn 
 				https://www.lainme.com/doku.php/topic/sage/start  # sage教程(good)
+				http://my.oschina.net/cvnote/blog/163094
+				http://download.csdn.net/detail/cr6128/2228103
+				http://mirror.hust.edu.cn/sagemath/src/index.html
+
 		# sagemath 云　(在线数学编辑,可用github登录) *******
 				https://cloud.sagemath.com   
