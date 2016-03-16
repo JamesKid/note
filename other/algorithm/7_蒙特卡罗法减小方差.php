2f72c74f1b134ae7619ce1c93a7a53d187e6d60c
JamesKid <406358964@qq.com>
2016-03-16 10:05:34 +0800

    add 蒙特卡罗减小方差的方法

---
+++ other/algorithm/7_蒙特卡罗法减小方差.php
@@ -0,0 +1,36 @@
+<?php
+/* 
+ * title:		 蒙特卡罗减小方差方法( Monte Carlo variance reduction method)
+ * description:  蒙特卡罗减小方差的方法，
+ * 算法评价:     简单粗暴，无招胜有招
+ * web:          
+ * 减小方差方法:  
+ *		web:
+ *			http://www.docin.com/p-194634586.html
+ *		tips:
+ *				1.controlvariates (控制变量法) 选择合理的变量变化范围，可以减小方差。
+ *				2.antithetic variates (对偶变量法) 利用相关联，或对称的点，可以减小方差。
+ *				3.importance sampling (重要抽样法) 找出一个函数式，在这个函数式上生成抽样点。可以减小方差.
+ *				4.stratified sampling (分层抽样法) 就是将抽样范围进行分割或分层抽样,然后加起分割的抽样等于总抽样
+ *		meaning: 减小方差的意义重大,方差越小表明波动越小,距离精确值越近
+ *
+ * slove:       1. 
+ */
+
+/* 　1. controlvariates(控制变量法)
+ *   
+ */
+
+/* 　2. antithetic variates(对偶变量法)
+ *   
+ */
+
+/* 　3. importance sampling (重要抽样法)
+ *   
+ */
+
+/* 　4. stratified sampling(分层抽样法)
+ *   
+ */
+?>
+
