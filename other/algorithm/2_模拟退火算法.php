1f3ea17fc8744b794fbfd49ea2f485f0731b7685
JamesKid <406358964@qq.com>
2016-04-05 15:44:26 +0800

    add 二分法算平分根

--- other/algorithm/2_模拟退火算法.php
+++ other/algorithm/2_模拟退火算法.php
@@ -3,6 +3,7 @@
  * title:		 模拟退火算法
  * description:  模拟退火算法
  * web:          http://blog.csdn.net/acdreamers/article/details/10019849
+ *				 http://www.cnblogs.com/heaad/archive/2010/12/20/1911614.html
  * slove:       1. 费马点问题
  *				2. 函数最值问题求解
  *				3. tsp问题求解
---
+++ other/algorithm/8_遗传算法.php
@@ -0,0 +1,10 @@
+<?php
+/* 
+ * title:		 遗传算法
+ * description: 
+ * web:          http://www.cnblogs.com/heaad/archive/2010/12/23/1914725.html
+ * slove:       1. 旅行商问题
+ *
+ */
+ */
+?>
