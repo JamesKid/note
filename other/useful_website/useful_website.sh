1e5043a0b05a48112147d32c5bfd080ab93c4ac0
JamesKid <406358964@qq.com>
2016-04-28 10:47:27 +0800

    add 菜鸟工具,菜鸟教程

--- other/useful_website/useful_website.sh
+++ other/useful_website/useful_website.sh
@@ -2,22 +2,27 @@
 #
 
 
-
 # computer
-	# web info
-		http://www.293.net     # get the value of the website
-		http://uptime.netcraft.com/up/graph?site=www.baidu.com   # get the information of the www.baidu.com
+		# web info
+				http://www.293.net     # get the value of the website
+				http://uptime.netcraft.com/up/graph?site=www.baidu.com   # get the information of the www.baidu.com
 
-	# web server
-		http://www.anilcetin.com/convert-apache-htaccess-to-nginx/    # converapache htaccess rewrite 
+		# web server
+				http://www.anilcetin.com/convert-apache-htaccess-to-nginx/    # converapache htaccess rewrite 
 																 	  # rules to  nginx
-	# vim 
-		http://vimcasts.org/      # here are some moveis  intruduce the vim plugin
+		# vim 
+				http://vimcasts.org/      # here are some moveis  intruduce the vim plugin
 	
-	# linux
+		# runoob
+				# 菜鸟工具 (here you acn debug a simple code , in any language, and some useful tips)
+						http://c.runoob.com/
+				# 菜鸟教程 (some useful course)
+						http://www.runoob.com/
+		
+		# linux
 
-	# security 
-		# password
-			# md5
-				http://aiisoo.com		# slove md5 code
+		# security 
+				# password
+						# md5
+						http://aiisoo.com		# slove md5 code
 
