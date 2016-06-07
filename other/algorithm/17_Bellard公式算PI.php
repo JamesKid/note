d1cb8ecfc87c3b329bf1a47536b8cddb19af1195
JamesKid <406358964@qq.com>
2016-06-07 10:26:26 +0800

    fix and add bellard method

---
+++ other/algorithm/17_Bellard公式算PI.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Description:
+ *		 http://m.csdn.net/article/2010-01-07/274539
+ *		 圆周率计算结果共需1137GB的硬盘容量，部分结果可以参见:http://bellard.org/pi/pi2700e9/pidigits.html
+ *		 二进制位计算时间为103天，验证花费13天。转换为十进制并验证又花费15天。总共时间为131天。
+ *		 二进制计算使用的是乌克兰Chudnovsky兄弟提出的算法，验证则使用了作者自己改进Bailey-Borwein-Plouffe算法后的公式（也称Bellard公式，是目前最快的圆周率算法，1997年提出）。
+ *
+ */
+?>
