b565a268c9aa45980f19160ecc7dad286ff8baa7
vimkidgit <1779156598@qq.com>
2016-08-15 18:06:24 +0800

    add install step(安装步骤)

--- other/linux/1_centos7_note.sh
+++ other/linux/1_centos7_note.sh
@@ -9,13 +9,21 @@
 				# web 
             http://wenku.baidu.com/link?url=VW3Xfubd3Cvhzzziq9a8m-j5r_V0CNv97tOKOVDRFCKOOX6zrq98buTO519XRBdO49P2gqAwnd37MHJl65P7UD3uz_8SYHdtIg4Y4-zEHeG
 
+
 				# tips 
             
 				    1. The filesystem format mast be fat32, NTFS can not be check 	
             2. 有的时候，读者会发现，在更新CentOS7之后，Windows7的引导项又没有了，没有关系。那是因为CentOS更新后，修改了grub.cfg这个文件。只要进CentOS7重新修改grub.cfg这个文件即可。比方作者出现这个问题时，“menuentry 'Windows 7 64_Bit' {”这句在更新后，后面多了一个“’”单引号这样的错误，去掉这个单引号，系统引导项正常。
 
         # detail 
-            # fix grub ( 修改主引导记录)
+            # 1. download
+                Wingrub_ha(分区查看软件)
+                EasyBSD(分区引导软件)
+                centos7-xxx.iso
+
+            # 2. 
+
+            #  fix grub ( 修改主引导记录)
                 # reboot press 'C' , into grub2 ,and press 'ls -l' to show windows7's position
                 # and you can find the position is (hd0,msdos1)
 
