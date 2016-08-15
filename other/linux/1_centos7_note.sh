722ddc55618e936c0c3550e08d90caa3fbefa3d3
vimkidgit <1779156598@qq.com>
2016-08-15 15:09:27 +0800

    add centos7 note

--- other/linux/1_centos7_note.sh
+++ other/linux/1_centos7_note.sh
@@ -10,16 +10,29 @@
             http://wenku.baidu.com/link?url=VW3Xfubd3Cvhzzziq9a8m-j5r_V0CNv97tOKOVDRFCKOOX6zrq98buTO519XRBdO49P2gqAwnd37MHJl65P7UD3uz_8SYHdtIg4Y4-zEHeG
 
 				# tips 
-				    The filesystem format mast be fat32, NTFS can not be check 	
+            
+				    1. The filesystem format mast be fat32, NTFS can not be check 	
+            2. 有的时候，读者会发现，在更新CentOS7之后，Windows7的引导项又没有了，没有关系。那是因为CentOS更新后，修改了grub.cfg这个文件。只要进CentOS7重新修改grub.cfg这个文件即可。比方作者出现这个问题时，“menuentry 'Windows 7 64_Bit' {”这句在更新后，后面多了一个“’”单引号这样的错误，去掉这个单引号，系统引导项正常。
 
         # detail 
+            # fix grub ( 修改主引导记录)
+                # reboot press 'C' , into grub2 ,and press 'ls -l' to show windows7's position
+                # and you can find the position is (hd0,msdos1)
+
+                vim /boot/groub2/grub.cfg # add under content
+                    menuentry 'Windows 7 64_Bit' { 
+                        insmod chain 
+                        insmod ntfs 
+                        set root=(hd0,msdos1)   # the disk area find by 'ls -l' before 
+                        chainloader +1 
+                    }
             
 # use 
 		# fix resolution ratio (修改分辨率)
 				# web 
 				    http://jingyan.baidu.com/article/2c8c281dad52af0008252adc.html
         # detail 
-            vim /boot/groub2/grub.cfg  
+            vim /boot/grub2/grub.cfg  
                 # find linux16 /vmlinux-3.10.  and add 'vga=ask' after this line
                 linux16 /vmlinuz-3.10.0-229.11.1.el7.x86_64 root=/dev/mapper/centos-root ro rd.lvm.lv=centos/root rd.lvm.lv=centos/swap crashkernel=auto rhgb quiet.utf8 vga=ask 
             shutdown -r now 
