d73868edaf24d60cde38d085b46070c02144887b
vimkidgit <1779156598@qq.com>
2016-08-17 11:18:21 +0800

    add kali linux note

---
+++ other/linux/2_kali_linux.sh
@@ -0,0 +1,11 @@
+# ================================JamesKid============================== 
+# @author       : JamesKid  
+# @description : This is note about kali linux operating system
+# @change       :
+# ======================================================================
+
+# web 
+    http://xiao106347.blog.163.com/blog/static/215992078201342410347137/  # 安装之后的简单设置 
+    https://www.kali.org    # 官网
+    http://blog.chinaunix.net/uid-26349264-id-4041727.html  # 实战使用
+
