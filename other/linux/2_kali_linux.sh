48a3909c2a744f3e547ad7c7eb46102ead48f722
vimkidgit <1779156598@qq.com>
2016-08-17 19:40:20 +0800

    fix kali note

--- other/linux/2_kali_linux.sh
+++ other/linux/2_kali_linux.sh
@@ -9,3 +9,37 @@
     https://www.kali.org    # 官网
     http://blog.chinaunix.net/uid-26349264-id-4041727.html  # 实战使用
 
+
+# install 
+# config (配置)
+
+    # update 
+        vim /etc/apt/sources.list 
+
+        #中科大kali源
+            deb http://mirrors.ustc.edu.cn/kali kali main non-free contrib
+            deb-src http://mirrors.ustc.edu.cn/kali kali main non-free contrib
+            deb http://mirrors.ustc.edu.cn/kali-security kali/updates main contrib non-free
+        #阿里云kali源
+            deb http://mirrors.aliyun.com/kali kali main non-free contrib
+            deb-src http://mirrors.aliyun.com/kali kali main non-free contrib
+            deb http://mirrors.aliyun.com/kali-security kali/updates main contrib non-free
+
+        apt-get update  # 刷新系统
+        apt-get dist-upgrade  # 安装更新
+
+    # vmware tools
+        # 1.请先执行update,upgrade
+        sudo apt-get install linux-headers-$(uname -r)  # 更新内核
+        # 3.点击虚拟机->安装vmare tools，进入系统后点击
+
+
+
+    # font error (字体重叠)    
+        apt-get install ttf-wqy-microhei ttf-wqy-zenhei xfonts-wqy  # 下载字体包
+        gnome-tweak-tool  # 命打开配置－>字体，选为文泉驿微变黑等宽
+        apt-get update && apt-get dist-upgrade
+
+
+        
+        
---
+++ other/secure/3_tools.sh
@@ -0,0 +1,25 @@
+# ================================JamesKid============================== 
+# @author       : JamesKid  
+# @description :  This is note about secure tools note 
+# @change       :
+# ======================================================================
+
+# 1 information collection (信息收集)
+
+# 2  (漏洞分析)
+
+# 3  (web程序)
+
+# 4  database testing (数据库评估软件)
+
+# 5  password (密码相关)
+
+# 6  wifi (wifi)
+
+# 7  reverse engineering (逆向工程)
+
+# 8  vulnerability exploitation (漏洞利用)
+
+# 9 
+
+
