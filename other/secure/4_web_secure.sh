7b2faeebf07e910f24bdb13e582d6db4b0efeba7
vimkidgit <1779156598@qq.com>
2016-08-29 16:58:37 +0800

    add web secure set

---
+++ other/secure/4_web_secure.sh
@@ -0,0 +1,19 @@
+# ================================JamesKid============================== 
+# @author       : JamesKid  
+# @description : This is note about web secure 
+# @change       :
+# ======================================================================
+
+# About server 
+    # hide server info(隐藏header server)
+        vim fastcgi.conf
+            # fastcgi_param SERVER_SOFTWARE nginx/$nginx_version; # 将这行改为如下
+            fastcgi_param SERVER_SOFTWARE myserver;  
+
+# About php
+    # hide php version
+        vim php.ini
+            expose_php = Off # 将expose_php = On 改为Off
+            
+            
+    
