414d0708eae07c93f465547108c281e1a9f9f795
vimkidgit <1779156598@qq.com>
2016-09-25 13:55:04 +0800

    add matlab note

---
+++ math/matlab/tips.sh
@@ -0,0 +1,25 @@
+# ================================JamesKid============================== 
+# @author       : JamesKid  
+# @description  : This is note about matlab
+# @change       :
+# ======================================================================
+
+# Command Window
+    # command 
+        clc           # 清屏
+        format short  # (以默认短形输出－单次有效)
+        clear b       # 清空b变量
+        clear         # 清空整个workspace 内存内容
+        doc           # 打开帮助文档
+        help          # 打开帮助文档
+    # tips
+        1. 用分号分割句子
+            >> r=2;
+            >> S=pi*r^2
+        2. 默认只显示5位有效数字(可以在file->preferences->commandwindow->textdisplay->long->ok->重启工具)
+        3. 按上下左右，可以查看以前输入过的命令
+        4. 紧密显示(可以在file->preferences->commandwindow->textdisplay->compact->ok->重启工具)
+
+
+
+
