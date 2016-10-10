99b089321f3d374fe1b1cbcee9bd4acd30a23c86
vimkidgit <1779156598@qq.com>
2016-10-10 14:32:53 +0800

    add 冒泡排序

--- other/algorithm/1_排序算法/1_冒泡排序.php
+++ other/algorithm/1_排序算法/1_冒泡排序.php
@@ -9,6 +9,9 @@
 *
 * @序列变化     排序原序列: 8 7 3 2 5 9 4 11
 *               第一次排序: 7 3 2 5 8 4 11 9  
+*               第二次排序: 3 2 5 7 4 8 9 11 
+*               第三次排序: 2 3 5 4 7 8 9 11  
+*               第四次排序: 2 3 4 5 7 8 9 11 
 *
 * @算法复杂度   : 时间复杂度 
 *                   最优　O(n)
@@ -27,8 +30,6 @@
 *                   最优： N-1       (次)
 *                   最坏： N*(N-1)   (次)
 *                   平均： N*(N-1)/2 (次)
-*               
-*
 *
 * @算法优点  1. 比较稳定
 *            2. 算法简单
@@ -40,12 +41,40 @@
 * @具体应用  1. 多人排队游戏
 *
 * ================================ www.vimkid.com =================================
+
  */
 
-/* 冒泡排序代码
- * @params numberList  数字列表
- */
-$numberList = array();
 
+class BubbleSort {
+    /* 
+     * 1. 原始冒泡排序代码(原始)
+     * @params numberList  数字列表
+     */
+    public function basic(){
+        $numberList = array(8,7,3,2,5,9,4,11); // 原始列表
+        $numberLength = count($numberList);    //获取数组长度
+        for( $i=0 ;$i<count($numberList)-1;$i++){ // 循环N-1次
+            for( $j=0; $j<$numberLength-1; $j++){   // 内部两两循环N-1次
+                if( $numberList[$j] > $numberList[$j+1]){ // 比较大小
+                    $tmp = $numberList[$j];  // 将交换值临时存放
+                    $numberList[$j] = $numberList[$j+1];   //交换值
+                    $numberList[$j+1] = $tmp;  // 交换值
+                } 
+            }
+            $times = "第".($i+1).'次结果:  ';  
+            echo $times;
+            foreach($numberList as $k => $v){
+                echo $v.' '; // 输出第一次结果序列
+            }
+            echo "\n";
+        }
+        echo "最终结果:   ";
+        foreach($numberList as $k => $v){
+            echo $v.' ';  // 输出最终结果
+        }
+    }
+}
+$bubble = new BubbleSort(); // 实列化
+$bubble->basic();  // 原始冒泡排序
 
 ?>
