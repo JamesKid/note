f65281929d9703f62443fcd5ba33966bb2f0895d
vimkidgit <1779156598@qq.com>
2017-09-14 14:04:36 +0800

    添加gnome3一刻

---
+++ other/linux/gnome.sh
@@ -0,0 +1,25 @@
+# ================================JamesKid============================== 
+# @author      : JamesKid  
+# @description : This is not about gnome3
+# @change      :
+# ======================================================================
+
+# install 
+
+# use
+    # version (查看版本)
+        gnome-session --version
+
+    # themes 更换主题
+        # download
+            https://www.gnome-look.org
+        # install
+            1. 将包解压放到/usr/share/themes
+            2. gnome-tweak-tool 选中相应主题
+
+    # inut method
+        # 五笔
+            yum search wubi
+            yum install ibus-table-chinese-wubi-jidian.noarch
+            设置->区域和语言->输入源->添加->汉语->五笔
+
