d7f4c1a709db6bdc3a62dc398e8fe26e3b44ef80
vimkidgit <1779156598@qq.com>
2017-10-16 10:37:59 +0800

    add engligh note

---
+++ other/english/1_want_to_say.sh
@@ -0,0 +1,32 @@
+1. What chat tools use in your country?   # my 
+    What kind of chat tools do you use in your country    #  what kind of sth  ***** 什么类型
+
+2. He think I am a teacher  
+    He thought I was a teacher     # 他以为我是老师        
+    He thought I was a teacher at ChongQing University     #  Sb thought  **** 某人以为
+
+3. I thought you are a Chinese
+    I thought you were Chinese                    # Sb thought
+
+4. Most people are known Engligh
+    Most people understand Engligh   # 大部分人懂英语
+
+5. Most speaker English is good
+    Most of the speakers have good English   # most of the *****   大部分
+
+6. Take your time   # 慢慢来      Take Sb's time  
+
+7. Why not a keyboard  # 为什么不是键盘      Why not sth
+
+8. East or west, Guilin scenery is the best.  #   ['sinəri] 风景
+    Guilin landscape tops those elsewhere.
+
+9. So embarrassing   # 好尴尬
+    Very awkward
+
+10. He speak English fluently
+    
+
+
+
+
---
+++ other/english/2_good_sentences.sh
@@ -0,0 +1,2 @@
+1. Let your life easy and happy.     #  By vBirds 2017.10.16
+
