f05e1d9f70e24bbbc937f4d1dcb34c8916817ab8
vimkidgit <1779156598@qq.com>
2018-03-30 11:05:28 +0800

    add fie

---
+++ other/javascript-3d/treejs/1_usetips.sh
@@ -0,0 +1,5 @@
+# useful website
+    http://www.hewebgl.com/article/articledir/1   # 教程
+    https://www.reddit.com/r/threejs/      # treejs 3d 模型
+    https://github.com/luosijie/threejs-examples   # 一个很不错的treejs 实现demo ******* 
+    https://github.com/luosijie/threejs-examples/jump   # treejs 实现 微信跳一跳demo 
